b45f399efae5965441fb4e7ee0976b296f47d754
Clara Dinato <dinatoclara@gmail.com>
2023-01-18 18:12:27 -0300

    adicionando itens no componente Setup e refatorando Header

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -7,19 +7,23 @@ export default function Setup() {
 			<Title text="Setup" />
 			{Object.entries(setup).map(([category, items]) => (
 				<div key={category}>
-					<h2 className="text-xl mt-6 mb-4 font-medium border-b-2 border-indigo-500 w-min">{category}</h2>
+					<h2 className="text-xl mt-6 mb-4 font-medium border-b-2 border-indigo-500 w-min">
+						{category}
+					</h2>
 					<ul className="mt-2">
 						{items.map((item) => (
 							<li key={item.name} className="mb-1 ml-6 list-disc">
-								<a className="hover:text-indigo-600 transition-all duration-300" href={item.link}>{item.name}</a>
+								<a
+									className="hover:text-indigo-600 transition-all duration-300"
+									href={item.link}
+								>
+									{item.name}
+								</a>
 							</li>
 						))}
 					</ul>
 				</div>
 			))}
-
-			{/* 
-			> */}
 		</section>
 	);
 }
